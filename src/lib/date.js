e68fd6a24fb9d00991d3a7bd811bad793e3aff70
taylor-w <taylorwood39@gmail.com>
2020-12-04 18:51:24 -0600

    Add date function

--- src/lib/date.js
+++ src/lib/date.js
@@ -1,5 +1,6 @@
 const header = document.getElementById("journal-header");
 
+// used to track date for journal entry
 export default () => {
   const dt = new Date();
   header.innerHTML = dt.toLocaleDateString();
