50d051b889352012a60d6b5d84637f747bff1a30
taylor-w <taylorwood39@gmail.com>
2020-12-04 23:42:16 -0600

    Add db folder and config connect

---
+++ db/config.js
@@ -0,0 +1,10 @@
+export default {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  apiKey: "AIzaSyDkJwOCF-kdS8jyEPU0ts1D-vN-ZqPwOkM",
+  authDomain: "ctrllog-76bb7.firebaseapp.com",
+  projectId: "ctrllog-76bb7",
+  storageBucket: "ctrllog-76bb7.appspot.com",
+  messagingSenderId: "953516577855",
+  appId: "1:953516577855:web:6ac4afdb5b877730b1548a",
+  measurementId: "G-CZJJGMX0X7",
+};
