caf9f179ca427393b1a09e9909a9ba237311b485
taylor-w <taylorwood39@gmail.com>
2020-12-06 13:33:45 -0600

    Add function to change background image utilizing splash api

--- components/form/theme.js
+++ components/form/theme.js
@@ -1,8 +1,13 @@
 const input = document.getElementById("img-text");
 const btn = document.getElementById("img-btn");
+let counter = 0;
 
 export default () => {
   btn.addEventListener("click", () => {
-    console.log(input.value);
+    document.documentElement.style.setProperty(
+      "--splash-img",
+      `url(https://source.unsplash.com/1920x1080/?${input.value}${counter})`
+    );
+    counter += 1;
   });
 };
