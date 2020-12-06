8b6e5f9604b5e9229105515e7928cb0f60817fe1
taylor-w <taylorwood39@gmail.com>
2020-12-06 16:53:01 -0600

    Remove console logs

--- components/settings/bgColor.js
+++ components/settings/bgColor.js
@@ -5,14 +5,11 @@ const bSlider = document.getElementById("j-bg-b");
 export default () => {
   rSlider.addEventListener("input", () => {
     document.documentElement.style.setProperty("--journal-r", rSlider.value);
-    console.log(rSlider.value);
   });
   gSlider.addEventListener("input", () => {
     document.documentElement.style.setProperty("--journal-g", gSlider.value);
-    console.log(gSlider.value);
   });
   bSlider.addEventListener("input", () => {
     document.documentElement.style.setProperty("--journal-b", bSlider.value);
-    console.log(bSlider.value);
   });
 };
