53d8094e896cda20bbb2261cddc1098736c4f272
taylor-w <taylorwood39@gmail.com>
2020-12-06 21:58:16 -0600

    Add toggle button functionality for keyword 'settings'

--- components/settings/settingsBtn.js
+++ components/settings/settingsBtn.js
@@ -1,9 +1,14 @@
 const btn = document.getElementById("settings-btn");
-const settings = document.getElementById("settings");
+const settingsWord = document.querySelector("h4");
+const settingsMenu = document.getElementById("settings");
 
 export default () => {
   btn.addEventListener("click", () => {
-    settings.classList.replace("is-collapsed", "is-expanded") ||
-      settings.classList.replace("is-expanded", "is-collapsed");
+    settingsMenu.classList.replace("is-collapsed", "is-expanded") ||
+      settingsMenu.classList.replace("is-expanded", "is-collapsed");
+  });
+  settingsWord.addEventListener("click", () => {
+    settingsMenu.classList.replace("is-collapsed", "is-expanded") ||
+      settingsMenu.classList.replace("is-expanded", "is-collapsed");
   });
 };
