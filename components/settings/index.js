964b620634e3ef02b99b8467ce70c924c30d37b6
taylor-w <taylorwood39@gmail.com>
2020-12-06 21:11:14 -0600

    Import textColor function

--- components/settings/index.js
+++ components/settings/index.js
@@ -1,7 +1,9 @@
-import themeFxn from "./theme.js";
 import bgColor from "./bgColor.js";
+import textColor from "./textColor.js";
+import themeFxn from "./theme.js";
 
 export default () => {
-  themeFxn();
   bgColor();
+  themeFxn();
+  textColor();
 };
